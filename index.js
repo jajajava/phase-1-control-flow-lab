9794a3d97000f419015a9a43db959608847334df
David <davidstepanov@yahoo.com>
2022-08-29 11:13:19 -0400

    commit tree 3502059de8f8e72bab7006878e23405b8113a928

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    console.log("This one is on me!")
+  } else if (someValue > 400 && someValue < 2000){
+    console.log("That will be twenty bucks.")
+  } else if (someValue > 2000 && someValue < 2500){
+    console.log("I will gladly take your thirty bucks.")
+  } else if (someValue > 2500){
+    console.log("No can do.")
+  }
 }
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(1500)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
